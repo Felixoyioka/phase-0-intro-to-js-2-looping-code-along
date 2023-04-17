4a46b1da10a9c6ed4a6cff3d0b8116af0d5ddcb3
Felix Oyioka <felixoyioka@gmail.com>
2023-04-17 16:26:16 +0300

    Complete looping lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+    const messages=[];
+  for (let i = 0; i < names.length; i++) {
+    const message=`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+    messages.push(message)
+  }
+  return messages;
+};
+const result = writeCards(["Charlie","Samip","Ali"],"birthday");
+console.log(result);
+function countDown(num){
+    while(num>=0){
+        console.log(num);
+        num--;
+    }
+    
+}
+countDown(10);
